db31beaa6784ebbcf5ba0b78fad72c902439350b
Ryan Sands <sandsryanj@gmail.com>
2021-03-25 10:35:25 -0700

    updated pin assignments to correspond with thunderboard and modified loop so it can be run to test preliminary wiring

--- goomba_driving.py
+++ goomba_driving.py
@@ -2,7 +2,7 @@
 #   For use with the EduShields TriplerBaseboard
 #
 # Written by Ryan Sands (sandsryanj@gmail.com)
-#   v0.8 22-Mar-2021 Drafting of functions for sensors and classes for state machine
+#   v0.8 25-Mar-2021 Drafting of functions for sensors and classes for state machine
 
 import board
 from board import SCL, SDA
@@ -12,6 +12,12 @@ import time, busio
 import adafruit_lis3mdl # magnetometer
 import adafruit_irremote
 import adafruit_hcsr04  # sonar sensor
+from adafruit_motor import motor  # need to look into what i can do with this
+
+from numpy import cos
+from numpy import sin
+
+from goomba_state import state
 ''' probably unnecessary bluetooth stuff
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
@@ -20,20 +26,16 @@ from adafruit_ble.services.nordic import UARTService
 from adafruit_bluefruit_connect.packet import Packet
 from adafruit_bluefruit_connect.button_packet import ButtonPacket
 '''
-from adafruit_motor import motor  # need to look into what i can do with this
-
-from numpy import cos
-from numpy import sin
-
-from goomba_state import state
 
 '''
  we have 7 digital pins,  6 analog pins explicitly - D13, D12, D11, D10, D9, D6, D5, D2
  Analog available A0, A1, A2, A3, A4, A5 just take out IW
- gives tentative total of 15? pins at our disposal, no I2C pins
- Sck, MO, MI pins can be used for general purpose IO (GPIO),
- but these also have special function for communicating by Serial Peripheral Interface
+ gives tentative total of 13 pins at our disposal, no I2C pins
+ Sck, MO, MI pins can be used for general purpose IO (GPIO), but we use SPI
+ so IR stuff goes to other board
 '''
+#PIN_IR = board.A2 move these pins to the thunderboard
+#PIN_IRLED = board.A3
 
 # sensor input pins
 #PIN_SON_L0 = board.D11
@@ -47,13 +49,13 @@ from goomba_state import state
 #PIN_ENC_L1 = board.A1
 #PIN_ENC_R0 = board.D12
 #PIN_ENC_R1 = board.D13
-PIN_IR = board.A2
+
 encL = rotaryio.IncrementalEncoder(PIN_ENC_L0, PIN_ENC_L1)
 encR = rotaryio.IncrementalEncoder(PIN_ENC_R0, PIN_ENC_R1)
 
 sonarL = adafruit_hcsr04.HCSR04(trigger_pin=PIN_SON_L0, echo_pin=PIN_SON_L1) # sonar dist in cm
 sonarF = adafruit_hcsr04.HCSR04(trigger_pin=PIN_SON_F0, echo_pin=PIN_SON_F1)
-sonarR = adafruit_hcsr04.HCSR04(trigger_pin=PIN_SON_F0, echo_pin=PIN_SON_F1)
+sonarR = adafruit_hcsr04.HCSR04(trigger_pin=PIN_SON_R0, echo_pin=PIN_SON_R1)
 
 
 i2c = busio.I2C(SCL, SDA)
@@ -62,12 +64,10 @@ pulsein = pulseio.PulseIn(PIN_IR, maxlen=150, idle_state=True)
 decoder = adafruit_irremote.GenericDecode()
 
 # output pins
-PIN_MOTL0 = board.A4 #these share channels with on board rot enc so don't twist it while in use
+PIN_MOTL0 = board.A4 # we need tot take out the IW before we use this
 PIN_MOTL1 = board.A5
-PIN_MOTR0 = board.SCK # the pins for this motor need to change as we use SCK and MI and MO pin used with rasp pi
-PIN_MOTR1 = board.MI
-#PIN_IRLED = board.A3? may want to have it controlled for the freq or duty because of how the pulse library works?
-# dylan says he has another board so we can use that instead for this
+PIN_MOTR0 = board.A2 # these pins replacing IR stuff
+PIN_MOTR1 = board.A3
 
 motL0 = pwmio.PWMOut(PIN_MOTL0)
 motL1 = pwmio.PWMOut(PIN_MOTL1)
@@ -81,7 +81,6 @@ motR.FAST_DECAY = 1
 DUTY_MAX = 2**16-1
 blink_time = 0.1
 last_time = 0
-# LIS_MAX = 100 # idk how to interpret magnetometer yet. might want to use it to get close to original location?
 
 # States of state machine
 class forward_state(state):
@@ -108,15 +107,11 @@ class turn_state(state):
         noty= 2 
         stuff_here = 1
         testing_stuff = again + noty * stuff_here
-    
-    def stuff(thing):
-        coool = True
 
 
 class idling_state(state):
     def __init__(self):
         idk = again
-
 
 
 class state_machine(object):
@@ -132,34 +127,34 @@ def motor_level(level,mot): # input is range of percents, -100 to 100
     mot.throttle = float(level/100)
 
 # not sure about including duration cause i'll need it to keep checking
-# brings up issue of how do I reverse direction? prob need two pins for each motor
 def turning(direction):
     mag = 75
     direc = str(direction).upper()
     if direction == "LEFT":
         motor_level(mag, motL)
         motor_level(-mag, motR)
+        
     elif direction == "RIGHT":
         motor_level(mag, motR)
         motor_level(-mag, motL)
     else:
         print(direction,"is not a valid direction to turn.")
 
-def distance(enc_change0, enc_change1): # is there some way to use magentometer w/ this?
+def distance(enc_change0, enc_change1): # is there some way to use magnetometer w/ this?
     enc_change = (enc_change0 + enc_change1)/2
     dist = enc_change * constant
     return dist
 
-def angle(enc_change0, enc_change1, prior_ang=0): # is there some way to use magentometer w/ this to verify?
+def angle(enc_change0, enc_change1, prior_ang=0): # is there some way to use magnetometer w/ this to verify?
     enc_change = (enc_change0 + enc_change1)/2
     ang = enc_change * constant
     ang_rad = ang * (3.141592/180) + prior_ang
     return ang_rad
 
-def new_vect(ang, dist): # takes radians
+def new_vect(ang, dist): # takes radians and cm
     vect = []
-    vect[0] = dist*cos(ang)
-    vect[1] = dist*sin(ang)
+    vect[0] = float(dist) * cos(ang)
+    vect[1] = float(dist) * sin(ang)
     return vect
 
 '''
@@ -189,24 +184,28 @@ def fuzzy_pulse_compare(pulse1, pulse2, fuzzyness=0.1): #interpret matching sing
 def cliff_dist(ir_stuff):
     ir_stuff = 0
 
-def vector_store(vec_list,outfile):
+def vector_store(vec_list, outfile):
     # JSON stuff here?
     json = "jason"
 
 
 # Main loop
 vector_array = {}
-origins = [] #would this be easier with object oriented programming?
+origins = [] # would this part be easier with object oriented programming?
 i = 0
 
-#print('X:{0:10.2f}, Y:{1:10.2f}, Z:{2:10.2f} uT'.format(mag_x, mag_y, mag_z))
+
 while True:
-    origins[i][0] = lis3.magnetic
-    #origins[i][1] starts at (0,0) in 2D, will add vector calc next pass
+    if i == 0:
+        origins[i][1] = (0,0)
+    if event == "turning":
+        i += 1
+        origins[i][0] = lis3.magnetic
     mag_x, mag_y, mag_z = origins[i][0]
-    dists = [sonarL.distance,sonarF.distance,sonarR.distance]
+    dists = [sonarL.distance, sonarF.distance, sonarR.distance]
     distL, distF, distR = dists
     print("Distances: {:.2f}L {:.2f}F {:.2f}R (cm)".format(*dists))
-    print(origins[i])
+    print('Magnetometer: {0:10.2f}X {1:10.2f}Y {2:10.2f}Z uT'.format(mag_x, mag_y, mag_z))
+    print('Encoders: {0:10.2f}L {1:10.2f}R pulses'.format(encL.position, encR.position))
+
     time.sleep(0.25)
-    i += 1
